b77d42ff0535baeced80f4b5929cf7a117108df7
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-15 11:16:36 +0700

    render html view

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-	return 'Web App with Python Flask!'
+	# return 'Web App with Python Flask!'
+	name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
 
 app.run(host='0.0.0.0', port=81)
